aaa8bbdaae608a467ee081f1a207e5c0d752cd39
Mixu78 <kantola.miksu@gmail.com>
2022-07-20 17:23:07 +0300

    Translation implementation

---
+++ docs/i18n/fi.js
@@ -0,0 +1,26 @@
+export default {
+    mainPage: {
+        section1:
+            "Abitikku on helpoin tapa luoda Abitti-tikku. Ei enää harmaita hiuksia lukiolaisille, " +
+            "vaan helppokäyttöinen yhden klikkauksen asennusohjelma.",
+        section2:
+            'Abitikku lataa uusimman virallisen Abitti-levykuvan ja asentaa sen tikullesi, vain yhdellä klikkauksella. ' +
+            'Ohjelman tarjoavat ilmaiseksi muun muassa <a href="https://matikkaeditori.fi">Matikkaeditori.fin</a> kehittänyt ja ' +
+            '<a href="https://abitti.testausserveri.fi">Abitti Open Access-haavoittuvuuden</a> löytänyt tiimi.',
+        section3: 'Tukea on saatavilla <a href="https://discord.testausserveri.fi">Discord-palvelimellamme</a>. <span id="versionText"></span>',
+        footer1: 'Tämä projekti on myös julkaistu avoimena lähdekoodina <a href="https://github.com/testausserveri/abitikku">GitHubiin</a>.',
+        footer3: "Ohjevideon on tehnyt Misha Lindvall.",
+        version: "Abitikun uusin julkaistu versio on ",
+        loadFail:
+            "Valitettavasti versioiden hakeminen epäonnistui. Pyydämme sinua ottamaan yhteyttä Abitikun tukeen.",
+    },
+    download: {
+        download: "Lataa ",
+        ubuntu: "Abitikku, Ubuntu/Debian (.deb)",
+        arch: "Abitikku, Arch (.rpm)",
+        linux: "Abitikku, Linux (.AppImage)",
+        windows: "Windowsille",
+        macos: "macOS:lle",
+        otherDownloads: "Muut latausvaihtoehdot",
+    },
+};
--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,5 @@
+import { getTranslation } from "./i18n/index.js";
+
 const platformIcons = {
     exe: "fa-windows",
     app: "fa-apple",
@@ -6,59 +8,95 @@ const platformIcons = {
     appImage: "fa-linux"
 }
 
+function getLang() {
+    if (window.localStorage.getItem("lang")) {
+        return window.localStorage.getItem("lang");
+    }
+    if (window.navigator.languages !== undefined && window.navigator.languages.length > 0) {
+        return window.navigator.languages[0].split("-")[0].toLowerCase();
+    }
+    return window.navigator.language.split("-")[0].toLowerCase();
+}
+
+function setLang(lang) {
+    window.localStorage.setItem("lang", lang)
+}
+
+function changeLang(lang) {
+    setLang(lang);
+    updateMainPage();
+}
+
+window.changeLang = changeLang;
+
+function updateMainPage() {
+    if (!window.abitikkuReleases) return;
+    const lang = getLang();
+    document.getElementById("section1").innerText = getTranslation(lang, "mainPage.section1");
+    document.getElementById("section2").innerHTML = getTranslation(lang, "mainPage.section2");
+    document.getElementById("section3").innerHTML = getTranslation(lang, "mainPage.section3");
+
+    document.getElementById("footer1").innerHTML = getTranslation(lang, "mainPage.footer1");
+    document.getElementById("footer3").innerText = getTranslation(lang, "mainPage.footer3");
+
+    updateLinks(window.abitikkuReleases.tag_name, window.abitikkuReleases.downloadLinks);
+}
+
 // what the fuck is style of the following code
 // burn it 😭
 function updateLinks(version, downloadLinks) {
+    const lang = getLang();
     Sniffr.sniff(window.navigator.userAgent);
     let osInfo = Sniffr.os;
     let url = downloadLinks.exe;
-    let nameInfo = "Lataa ";
+    let nameInfo = getTranslation(lang, "download.download");
     let icon = "fa-windows";
 
     if (osInfo.name === "linux") {
         url = downloadLinks.appImage,
             icon = platformIcons.appImage;
 
-        if (window.navigator.userAgent.toLowerCase().includes("ubuntu") ||  window.navigator.userAgent.toLowerCase().includes("deb")) {
+        if (window.navigator.userAgent.toLowerCase().includes("ubuntu") || window.navigator.userAgent.toLowerCase().includes("deb")) {
             url = downloadLinks.deb,
                 icon = platformIcons.deb,
-                nameInfo += "Abitikku, Ubuntu/Debian (.deb)";
+                nameInfo += getTranslation(lang, "download.ubuntu");
         } else if (window.navigator.userAgent.toLowerCase().includes("arch")) {
             url = downloadLinks.rpm,
                 icon = platformIcons.rpm,
-                nameInfo += "Abitikku, Arch (.rpm)";
+                nameInfo += getTranslation(lang, "download.arch");
         } else {
             url = downloadLinks.appImage,
                 icon = platformIcons.appImage,
-                nameInfo += "Abitikku, Linux (.AppImage)";
+                nameInfo += getTranslation(lang, "download.linux");
         }
     } else if (osInfo.name === "windows") {
         url = downloadLinks.exe,
             icon = platformIcons.exe,
-            nameInfo += "Windowsille";
+            nameInfo += getTranslation(lang, "download.windows");
     } else if (osInfo.name === "macos") {
         url = downloadLinks.app,
             icon = platformIcons.app,
-            nameInfo += "macOS:lle";
+            nameInfo += getTranslation(lang, "download.macos");
     }
-    
 
     let downloadText = document.getElementById("downloadText");
     let downloadLink = document.getElementById("downloadLink");
+    let otherDownloads = document.getElementById("otherDownloads");
     let versionText = document.getElementById("versionText");
     let downloadIcon = document.querySelector(".fab");
 
     downloadIcon.className = `fab ${icon}` // clearing out the initial icon
-    
+
     downloadLink.href = url;
     downloadText.innerHTML = nameInfo;
-    versionText.innerHTML = `Abitikun uusin julkaistu versio on ${version}.`;
+    versionText.innerHTML = `${getTranslation(lang, "mainPage.version")}${version}.`;
+    otherDownloads.innerText = getTranslation(lang, "download.otherDownloads");
 }
 
 fetch("https://api.github.com/repos/testausserveri/abitikku/releases/latest")
     .then(res => res.json())
     .then(({ tag_name, assets }) => {
-        if (!assets) alert("Valitettavasti versioiden hakeminen epäonnistui. Pyydämme sinua ottamaan yhteyttä Abitikun tukeen.")
+        if (!assets) alert(getTranslation(getLang(), "mainPage.loadFail") || "Valitettavasti versioiden hakeminen epäonnistui. Pyydämme sinua ottamaan yhteyttä Abitikun tukeen.")
 
         let downloadLinks = {
             exe: assets.find(asset => asset.name.includes("Setup"))["browser_download_url"],
@@ -70,5 +108,11 @@ fetch("https://api.github.com/repos/testausserveri/abitikku/releases/latest")
 
         console.log(`Found the following download links from the latest release (${tag_name})`, downloadLinks)
 
+        window.abitikkuReleases = {
+            tag_name,
+            downloadLinks
+        };
+
         updateLinks(tag_name, downloadLinks)
+        updateMainPage()
     })
